ad5fdd733eaf934640b11df916e19fecb99edbc4
shinework <adrien.baptiste@gmail.com>
2017-05-09 23:54:23 +0200

    Handle add message

--- src/components/MessageBar.js
+++ src/components/MessageBar.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 const inputStyle = {borderRadius: 10, border: 'none', fontSize: 16, padding: 10, marginRight: 10, flexGrow: 1};
 const buttonStyle = {borderRadius: 10, cursor: 'pointer', fontSize: 20, color: '#336E7B', fontWeight: 'bold', padding: 10, flexGrow: 1, border: '3px solid #336E7B', backgroundColor: 'transparent'};
 
-const MessageBar = () => {
-  return (
-    <div style={{display: 'flex', padding: 10}}>
-      <input placeholder="Votre message" type="text" style={inputStyle} />
-      <br />
-      <button style={buttonStyle}>Envoyer</button>
-    </div>
-  )
+class MessageBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {message: ''};
+  }
+
+  handleChange(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  handleSubmit() {
+    if (this.state.message !== '') {
+      this.props.handleMessageSubmit(this.state.message);
+      this.setState({ message: '' });
+    }
+  }
+
+  render() {
+    return (
+      <div style={{display: 'flex', padding: 10}}>
+        <input value={this.state.message} onChange={this.handleChange} placeholder="Votre message" type="text" style={inputStyle} />
+        <br />
+        <button onClick={this.handleSubmit} style={buttonStyle}>Envoyer</button>
+      </div>
+    )
+  }
 }
+
+MessageBar.propTypes = {
+  handleMessageSubmit: PropTypes.func.isRequired,
+};
 
 export default MessageBar;
