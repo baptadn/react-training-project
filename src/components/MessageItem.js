abd86505ab98c9566eea6228cab381239a47588d
shinework <adrien.baptiste@gmail.com>
2017-05-10 23:57:17 +0200

    Add emoji support

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Emojione } from 'react-emoji-render';
+
+
 
 const MessageList = ({message}) => {
   return (
     <li style={{ marginBottom: 10, backgroundColor: 'white', padding: 10, borderRadius: 10}}>
       <small>@{message.username}</small>
-      <div>{message.body}</div>
+      <div><Emojione text={message.body} /></div>
     </li>
   );
 }
