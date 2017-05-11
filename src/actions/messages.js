84bac82e13572c90ed2067ca4cebb9f170567e37
shinework <adrien.baptiste@gmail.com>
2017-05-11 22:27:29 +0200

    Added messages load api

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {ADD_MESSAGE_ACTION, REMOVE_MESSAGE_ACTION} from '../constants/action';
+import axios from 'axios';
+import * as action from '../constants/action';
 
 export const addMessage = (message, username) => {
   return {
-    type: ADD_MESSAGE_ACTION,
+    type: action.ADD_MESSAGE_ACTION,
     message,
     username,
   };
@@ -11,7 +12,23 @@ export const addMessage = (message, username) => {
 
 export const removeMessage = index => {
   return {
-    type: REMOVE_MESSAGE_ACTION,
+    type: action.REMOVE_MESSAGE_ACTION,
     index,
+  };
+};
+
+export const loadMessages = () => {
+  return (dispatch) => {
+    dispatch({type: action.LOAD_MESSAGES_PENDING_ACTION});
+
+    return axios.get('http://www.mocky.io/v2/5914c6af100000ae0f9a5c1a')
+      .then(response => {
+        dispatch({
+          type: action.LOAD_MESSAGES_SUCCESS_ACTION,
+          messages: response.data.messages
+        });
+      }).catch(error => {
+        dispatch({type: action.LOAD_MESSAGES_ERROR_ACTION, error: error});
+      });
   };
 };
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import MessageItem from './MessageItem';
+import {loadMessages} from './../actions/messages';
 import {connect} from 'react-redux';
 
-const MessageList = ({messages}) => {
-  return (<ul style={{borderColor: 'grey', padding: 10, listStyle: 'none'}}>
-    {messages.map((message, i) => {
-      return <MessageItem key={i} index={i} message={message} />
-    })}
-    {messages.length === 0 && <li>Pas de message 🎈</li>}
-  </ul>)
+class MessageList extends Component {
+  componentDidMount() {
+    this.props.loadMessages();
+  }
+
+  render() {
+    return (<ul style={{borderColor: 'grey', padding: 10, listStyle: 'none'}}>
+      {this.props.messages.map((message, i) => {
+        return <MessageItem key={i} index={i} message={message} />
+      })}
+      {this.props.pending && <li>Chargement 🚀</li>}
+      {!this.props.pending && this.props.messages.length === 0 && <li>Pas de message 🎈</li>}
+    </ul>)
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    messages: state.message.messages
+    messages: state.message.messages,
+    pending: state.message.pending,
   }
 };
 
-const connectComponent = connect(mapStateToProps);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadMessages: () => {
+      dispatch(loadMessages())
+    }
+  }
+};
+
+const connectComponent = connect(mapStateToProps, mapDispatchToProps);
 
 export default connectComponent(MessageList);
